758337b419ca221b8a83c3cc7cd8086c7eac5a66
Iofem <iofemark2@gmail.com>
2024-02-06 21:22:40 +0500

    you can click on the placemarks on the map

--- app/src/main/java/com/example/sport_spot/MainActivity2.java
+++ app/src/main/java/com/example/sport_spot/MainActivity2.java
@@ -46,7 +46,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         // Список координат для меток
         Point[] points = {
-                new Point(56.829674, 60.634672),
+                new Point(56.829674, 60.634673),
                 new Point(56.826192, 60.621252),
                 new Point(56.833092, 60.631720),
                 new Point(56.832533, 60.626737)
