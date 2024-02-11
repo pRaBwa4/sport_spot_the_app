0d6b7755c0ad09fd44cbd44a569d64d987270b6e
Artem <topprabwa@gmail.com>
2024-02-11 16:48:35 +0500

    Now after pressing basket buttons you will see its info in MainActivity3. Added basket_photo, 3 text views and color-changing button to MainActivity3.

--- app/src/main/java/com/example/sport_spot/MainActivity3.java
+++ app/src/main/java/com/example/sport_spot/MainActivity3.java
@@ -1,14 +1,47 @@
 package com.example.sport_spot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    private Button myButton;
+    private boolean isCancelled = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        // передаем инфу про адрес из MainActivity2
+        String address = getIntent().getStringExtra("ADDRESS");
+
+        // отображение адреса
+        TextView addressTextView = findViewById(R.id.addressTextView);
+        addressTextView.setText(address);
+
+        // Set up button
+        myButton = findViewById(R.id.iam_here_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleButtonTextAndColor();
+            }
+        });
+    }
+        // кнопка меняется в зависимости от нажатия
+    private void toggleButtonTextAndColor() {
+        if (isCancelled) {
+            myButton.setText("Буду тут!");
+            myButton.setBackgroundColor(getResources().getColor(android.R.color.black));
+        } else {
+            myButton.setText("Отменить");
+            myButton.setBackgroundColor(getResources().getColor(R.color.red_color_for_button_cancel));
+        }
+        isCancelled = !isCancelled;
     }
 }
