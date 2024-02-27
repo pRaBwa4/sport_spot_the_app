9b1596c162a185da2a23e9427648466abcb0a99c
Artem <topprabwa@gmail.com>
2024-02-27 20:05:51 +0500

    Now after pressing basket buttons you will see its info in MainActivity3. Added basket_photo, 3 text views and color-changing button to MainActivity3.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,12 @@ dependencies {
     implementation ("com.yandex.android:maps.mobile:4.4.0-lite")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth:22.3.1")
+    implementation ("com.google.android.gms:play-services-auth:21.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-firestore:24.10.2")
+
 }
