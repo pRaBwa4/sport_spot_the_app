17f272df559fb3abb77f172fef6c82fc906f46b9
Iofem <iofemark2@gmail.com>
2024-02-27 20:00:16 +0500

    Registration for re-entry after logging out does not work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-firestore:24.10.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -49,5 +48,6 @@ dependencies {
     implementation ("com.google.android.gms:play-services-auth:21.0.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore:24.10.2")
 
 }
--- app/src/main/java/com/example/sport_spot/MainActivity2.java
+++ app/src/main/java/com/example/sport_spot/MainActivity2.java
@@ -11,7 +11,6 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
